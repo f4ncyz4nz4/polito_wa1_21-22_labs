ea292bd85a828922b314d86f1befc3aad1ae0f65
ippo <s295502@studenti.polito.it>
2022-03-31 16:06:11 +0200

    modify files for the lab 3 adding js to html

--- FilmLibrary/film_library.js
+++ FilmLibrary/film_library.js
@@ -1,47 +1,37 @@
 "use strict";
 
+/*const dayjs = require("dayjs");
+
 const dayjs = require("dayjs");
 const isToday = require('dayjs/plugin/isToday');
 dayjs.extend(isToday);
 
-const sqlite = require("sqlite3");
+const sqlite = require("sqlite3");*/
 
 //lab01
 
-const arr = ["spring", "abcd", "cabina", "cat", "it"];
-let new_arr = [];
-
-function trans(arr, new_arr) {
-    new_arr = arr.map((element) => {
-        if (element.length < 2)
-            return "";
-        return element.slice(0, 2) + element.slice(-2);
-    });
-    return new_arr;
-}
-
-function Film(id, title, favourites = false, date, rating) {
+function Film(id, title, favorite = false, date, rating) {
     this.id = id;
     this.title = title;
-    this.favourites = favourites;
-    this.date = date && dayjs(date);
-    this.rating = rating;
+    this.favorite = favorite;
+    this.date = date === undefined ? "" : dayjs(date).format('YYYY-MM-DD');
+    this.rating = date === undefined ? "" : rating;
 }
 
 function FilmLibrary() {
     this.filmLibrary = [];
-    const db = new sqlite.Database("./films.db", (err) => {
+    /*const db = new sqlite.Database("./films.db", (err) => {
         if (err) {
             throw err;
         }
-    });
+    });*/
 
     this.addNewFilm = (film) => this.filmLibrary.push(film);
 
     this.print = () => {
         this.filmLibrary
             .forEach((film) => {
-                console.log(`Id: ${film.id}, Title: ${film.title}, Favourite: ${film.favourites}, Watch date: ${film.date}, Score: ${film.rating}`)
+                console.log(`Id: ${film.id}, Title: ${film.title}, Favourite: ${film.favorite}, Watch date: ${film.date}, Score: ${film.rating}`)
             })
     };
 
@@ -66,175 +56,75 @@ function FilmLibrary() {
 
     this.getRated = () => {
         this.filmLibrary.filter(film => film.rating > 0).sort((a, b) => a.rating - a.rating)
-            .forEach((film) => console.log(`Id: ${film.id}, Title: ${film.title}, Favourite: ${film.favourites}, Watch date: ${film.date}, Score: ${film.rating}`));
+            .forEach((film) => console.log(`Id: ${film.id}, Title: ${film.title}, Favourite: ${film.favorite}, Watch date: ${film.date}, Score: ${film.rating}`));
     };
 
     //lab02
+}
+//lab03
 
-    this.getAll = () => {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows.map((f) => new Film(f.id, f.title, f.favorite, f.watchdate, f.rating)));
-                }
-            });
+function generateContentofTable(lib, filter) {
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+    lib.forEach((film) => {
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `<td>${film.title}</td>
+        <td>${film.favorite}</td><td>${film.date}</td><td>${film.rating}</td>`;
+        tr.innerHTML += `<td><button type="button" class="btn btn-warning">
+        <img class="float-end" src="./icons/trash.svg" alt="" width="20" height="24"></img></button></td>`;
+
+        tbody.appendChild(tr);
+
+        const deleteButton = tr.querySelector("button");
+
+        deleteButton.addEventListener("click", (event) => {
+            lib = lib.filter((f) => f.id !== film.id);
+            generateContentofTable(lib, "All");
         });
-    };
-
-    this.getAllFavourite = () => {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows
-                        .filter((f) => (f.favorite))
-                        .map((f) => new Film(f.id, f.title, f.favorite, f.watchdate, f.rating)));
-                }
-            });
-        });
-    };
-
-    this.getWatchedToday = () => {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows
-                        .filter((f) => (dayjs(f.watchdate).isToday()))
-                        .map((f) => new Film(f.id, f.title, f.favorite, f.watchdate, f.rating)));
-                }
-            });
-        });
-    };
-
-    this.getWatchedEarlier = (date) => {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows
-                        .filter((f) => (dayjs(f.watchdate).isBefore(dayjs(date))))
-                        .map((f) => new Film(f.id, f.title, f.favorite, f.watchdate, f.rating)));
-                }
-            });
-        });
-    };
-
-    this.getRatedEqualOrMoreThan = (rate) => {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows
-                        .filter((f) => (f.rating >= rate))
-                        .map((f) => new Film(f.id, f.title, f.favorite, f.watchdate, f.rating)));
-                }
-            });
-        });
-    };
-
-    this.getTitle = (title) => {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows
-                        .filter((f) => (f.title === title))
-                        .map((f) => new Film(f.id, f.title, f.favorite, f.watchdate, f.rating)));
-                }
-            });
-        });
-    };
-
-    this.storeMovie = (film) => {
-        return new Promise((resolve, reject) => {
-            const sql = "INSERT INTO films (id, title, favorite, watchdate, rating) VALUES (?, ?, ?, ?, ?)";
-            db.run(sql, [film.id, film.title, film.favourites, film.date, film.rating], (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(true);
-                }
-            });
-        });
-    };
-
-    this.deleteMovie = (id) => {
-        return new Promise((resolve, reject) => {
-            const sql = "DELETE FROM films WHERE id=?";
-            db.run(sql, [id], (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(true);
-                }
-            });
-        });
-    };
-
-    this.deleteWatchDate = () => {
-        return new Promise((resolve, reject) => {
-            const sql = "UPDATE films SET watchdate = NULL";
-            db.run(sql, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(true);
-                }
-            });
-        });
-    };
-
+    });
+    const h2 = document.querySelector("h2");
+    h2.innerText = filter;
 }
 
-//-----------------------------------
+//main
+document.addEventListener("DOMContentLoaded", (event) => {
+    const f1 = new Film(1, "Pulp Fiction", true, "2021-03-10", 5);
+    const f3 = new Film(3, "Star Wars", false);
+    const f2 = new Film(2, "21 Grams", true, "2022-01-21", 4);
+    const f4 = new Film(4, "Matrix", true);
+    const f5 = new Film(5, "Shrek", false, "2022-03-17", 3);
+    const f7 = new Film(7, "Life is bad", true, "2022-11-21", 1);
 
-const f1 = new Film(1, "Pulp Fiction", true, "2021-03-10", 5);
-const f3 = new Film(3, "Star Wars", false);
-const f2 = new Film(2, "21 Grams", true, "2022-01-21", 4);
-const f4 = new Film(4, "Matrix", true);
-const f5 = new Film(5, "Shrek", false, "2022-03-17", 3);
-const f7 = new Film(7, "Life is bad", true, "2022-11-21", 1);
+    const flib = new FilmLibrary;
 
-const flib = new FilmLibrary;
+    flib.addNewFilm(f1);
+    flib.addNewFilm(f2);
+    flib.addNewFilm(f3);
+    flib.addNewFilm(f4);
+    flib.addNewFilm(f5);
 
-flib.addNewFilm(f1);
-flib.addNewFilm(f2);
-flib.addNewFilm(f3);
-flib.addNewFilm(f4);
-flib.addNewFilm(f5);
+    generateContentofTable(flib.filmLibrary, "All");
 
-flib.getAll().then((list) => {
-    console.log(list);
+    document.getElementById("all").addEventListener("click", (event) => {
+        generateContentofTable(flib.filmLibrary, "All");
+    });
+
+    document.getElementById("favorite").addEventListener("click", (event) => {
+        generateContentofTable(flib.filmLibrary.filter((f) => (f.favorite)), "Favourite");
+    });
+
+    document.getElementById("bestRated").addEventListener("click", (event) => {
+        generateContentofTable(flib.filmLibrary.filter((f) => (f.rating === 5)), "Best Rated");
+    });
+
+    document.getElementById("seenLastM").addEventListener("click", (event) => {
+        generateContentofTable(flib.filmLibrary.filter((f) => {
+            if (dayjs(f.date).isAfter(dayjs().subtract(30, "day")))
+                return true;
+            else
+                return false;
+        }), "Seen Last Month");
+    });
+
 });
-flib.getAllFavourite().then((list) => {
-    console.log(list);
-});
-flib.getWatchedToday().then((list) => {
-    console.log(list);
-});
-flib.getWatchedEarlier("2022-03-20").then((list) => {
-    console.log(list);
-});
-flib.getRatedEqualOrMoreThan(2).then((list) => {
-    console.log(list);
-});
-flib.getTitle("21 Grams").then((list) => {
-    console.log(list);
-});
-flib.storeMovie(f7).then(() => console.log("Succesfully inserted"));
-flib.deleteMovie(7).then(() => console.log("Succesfully deleted"));
-flib.deleteWatchDate().then(() => console.log("Succesfully modify watchdate"));
